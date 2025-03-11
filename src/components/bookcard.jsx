3aeacee1870cf9dfcbff3f07c33db0d0bc435b0f
vishalm342 <vishalm8656@gmail.com>
2025-03-11 10:25:07 +0530

    bookcard component added

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,19 @@
-// write the book component code here
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './bookcard.css';
+
+// eslint-disable-next-line react/prop-types
+const BookCard = ({ image, name, genre, author }) => {
+  return (
+    <div className="book-card">
+      <img src={image} alt={name} className="book-image" />
+      <div className="book-info">
+        <h3 className="book-title">{name}</h3>
+        <p className="book-author">by {author}</p>
+        <p className="book-genre">{genre}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
